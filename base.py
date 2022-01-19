16ae4838c503eb33e7c26f684003228654ddfec3
Gil <elbazg123@gmail.com>
2022-01-19 20:10:50 +0200

    got column names from aai

--- base.py
+++ base.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-import os
 
 PATH = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -30,53 +29,42 @@ def previous():
 
 for idx, url in enumerate(bbc_hrefs):
     driver.get(url)
-
-    # This part gets most of the articles.
-    '''header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
-    data_block = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
-    for block in data_block:
-        print(block.text)
-
-    content_dict[idx] = header.text
-    print(content_dict)
-    previous()'''
-
-    # This part gets the rest of the articles.
-    '''try:
+    header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
+    if header is None:
         header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
             (By.XPATH, "//div[@class='article-headline__text b-reith-sans-font b-font-weight-300']")))
         data_block = WebDriverWait(driver, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, "//div[@class='article__body-content']")))
         for block in data_block:
             print(block.text)
-
-        content_dict[idx] = header.text
-        print(content_dict)
         previous()
-    except:
-        print("else")
-        pass'''
+    else:
+        data_block = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
+        for block in data_block:
+            print(block.text)
+        previous()
 
+    '''content_dict[idx] = header.text
+    print(content_dict)
 
+    # This part gets the rest of the articles.
+    header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        (By.XPATH, "//div[@class='article-headline__text b-reith-sans-font b-font-weight-300']")))
+    data_block = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//div[@class='article__body-content']")))
+    for block in data_block:
+        print(block.text)
 
-'''for idx, val in enumerate(media_list):
-    #WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.block-link__overlay-link")))
-    time.sleep(1)
-    media_list = driver.find_elements_by_css_selector("a.block-link__overlay-link")
-    item = media_list[idx]
-    actions.move_to_element(item).perform()
-    time.sleep(0.5)
-    item.click()
-    # scrape your data
-    # print(item.get_attribute('href'))
-    driver.execute_script("window.history.go(-1)")
+    content_dict[idx] = header.text
+    print(content_dict)
+    previous()
 '''
 driver.close()
 driver.quit()
 
-class Base(webdriver.Chrome):
-    
+'''class Base(webdriver.Chrome):
+
     # Add previous
 
     def __init__(self, driver_path="C:/Program Files (x86)/chromedriver.exe", teardown=False):
@@ -91,6 +79,4 @@ class Base(webdriver.Chrome):
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
             self.quit()
-
-
-
+'''
--- iaa.py
+++ iaa.py
@@ -2,14 +2,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 
-# TODO fix status in first column - example CANCELED
-
-# //table[@id='flight_board-arrivel_table']//tbody//tr[@role='row']//div/*
-
-# //*[@id="flight_board-arrivel_table"]/tbody/tr/td/div/div[2]
+# TODO - Keep table updated
 
 PATH = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -17,6 +12,7 @@ driver.maximize_window()
 url = 'https://www.iaa.gov.il/en/airports/ben-gurion/flight-board/'
 
 driver.get(url)
+row_lists = []
 
 # Get the table headers
 table_headers = WebDriverWait(driver, 10).until(
@@ -32,10 +28,8 @@ for row in table.find_elements_by_css_selector('tr'):
     for cell in row.find_elements_by_tag_name('td'):
         print(cell.text)
 
+data_table = pd.DataFrame(columns=columns)
+print(data_table)
 
 driver.close()
 driver.quit()
-
-a = []
-
-
