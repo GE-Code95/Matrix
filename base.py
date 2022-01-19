fa6612a8c5367ad742152fefe1b4ebbd72f148ba
Gil <elbazg123@gmail.com>
2022-01-19 10:06:55 +0200

    finished BBC extraction part

--- base.py
+++ base.py
@@ -23,35 +23,42 @@ media_list = driver.find_elements_by_css_selector("a.block-link__overlay-link")
 for link in media_list:
     bbc_hrefs.append(link.get_attribute('href'))
 
-'''first = bbc_hrefs[0]
-driver.get(first)
-header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
-
-print(header.text)'''
-
 
 def previous():
     driver.execute_script("window.history.go(-1)")
 
-content = ""
+
 for idx, url in enumerate(bbc_hrefs):
     driver.get(url)
-    try:
-        header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
+
+    # This part gets most of the articles.
+    '''header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
+    data_block = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
+    for block in data_block:
+        print(block.text)
+
+    content_dict[idx] = header.text
+    print(content_dict)
+    previous()'''
+
+    # This part gets the rest of the articles.
+    '''try:
+        header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@class='article-headline__text b-reith-sans-font b-font-weight-300']")))
         data_block = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
+            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='article__body-content']")))
         for block in data_block:
             print(block.text)
 
         content_dict[idx] = header.text
-        print(content)
+        print(content_dict)
         previous()
+    except:
+        print("else")
+        pass'''
 
-    except:  # Handle different title html
-        print(url)
-        pass
 
-    print(content_dict)
 
 '''for idx, val in enumerate(media_list):
     #WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.block-link__overlay-link")))
