02d1bfe0b795f3f81163956720c98cfe2e0c4345
Gil <elbazg123@gmail.com>
2022-01-20 12:41:12 +0200

    fixed bbc extraction + added headless firefox

--- base.py
+++ base.py
@@ -7,6 +7,9 @@ from selenium.webdriver.firefox.options import Options
 import time
 import nltk
 
+nltk.download()
+
+counter = 0
 options = Options()
 options.headless = True
 options.binary_location = "C:/Program Files/Mozilla Firefox/firefox.exe"
@@ -18,7 +21,6 @@ driver.get(url)
 actions = ActionChains(driver)
 
 bbc_hrefs = []
-content_dict = {}
 
 WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.block-link__overlay-link")))
 time.sleep(1)
@@ -26,28 +28,40 @@ media_list = driver.find_elements_by_css_selector("a.block-link__overlay-link")
 
 for link in media_list:
     bbc_hrefs.append(link.get_attribute('href'))
+    print(link.get_attribute('href'))
 
 
 def previous():
     driver.execute_script("window.history.go(-1)")
 
 
-'''for idx, url in enumerate(bbc_hrefs):
-    try:
-        if 'sport' not in url:
-            driver.get(url)
-            header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
-            data_block = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
-            print(header.text)
-            for block in data_block:
-                print(block.text)
-        previous()
-    except:
-        pass'''
-
-
 for idx, url in enumerate(bbc_hrefs):
+    if 'sport' not in url and 'news' in url:
+        driver.get(url)
+        header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "main-heading")))
+        data_block = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, "//div[@data-component='text-block']")))
+        print(header.text)
+        for block in data_block:
+            print(block.text)
+    previous()
+else:
+    if 'sport' not in url and 'news' not in url:
+        driver.get(url)
+        header = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@tabindex='-1']")))
+
+        intro = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@class='article__intro b-font-family-serif']")))
+
+        body = WebDriverWait(driver, 10).until(EC.visibility_of_any_elements_located((By.XPATH, "//div/div/p")))
+        print(header.text)
+        print(intro.text)
+
+        for p in body:
+            print(p.text)
+
+'''for idx, url in enumerate(bbc_hrefs):
     try:
         if 'sport' not in url:
             driver.get(url)
@@ -59,6 +73,7 @@ for idx, url in enumerate(bbc_hrefs):
 
             body = WebDriverWait(driver, 10).until(EC.visibility_of_any_elements_located((By.XPATH, "//div/div/p")))
 
+            counter = counter + 1
             print(header.text)
             print(intro.text)
 
@@ -67,10 +82,9 @@ for idx, url in enumerate(bbc_hrefs):
     except:
         pass
 
+print(counter)'''
 driver.close()
 driver.quit()
-
-
 
 
 class Base(webdriver.Chrome):
@@ -80,7 +94,7 @@ class Base(webdriver.Chrome):
     def __init__(self, driver_path="C:/Program Files (x86)/chromedriver.exe", teardown=False):
         self.teardown = teardown
         self.driver_path = driver_path
-        #os.environ['PATH'] += self.driver_path
+        # os.environ['PATH'] += self.driver_path
         super(Base, self).__init__()
 
     def access_page(self, url):
@@ -89,6 +103,3 @@ class Base(webdriver.Chrome):
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
             self.quit()
-
-
-
